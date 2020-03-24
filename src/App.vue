85ab433786f1df28347ef269ab90c66140ab1947
Mohammad Ravand <githubmohammadrepo@gmail.com>
2020-03-24 20:07:53 +0430

    navbar-top completed in mobile devides

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <b-container fluid class="p-0">
+
+        <b-navbar type="dark" class="d-flex flex-row-reverse p-0" variant="light">
+          
+          <b-navbar-brand href="#" class="h3 text-dark  m-0 px-2 m-auto">
+            <b-icon icon="book"></b-icon>
+          </b-navbar-brand>
+
+            <b-input-group class="d-flex align-items-center max-width m-auto">
+              <b-input-group-prepend class="d-flex">
+                  <b-button variant="outline-success" >
+                    <b-icon icon="search"></b-icon>
+                  </b-button>
+              </b-input-group-prepend>
+
+              <b-form-input class="" type="text"></b-form-input>
+            </b-input-group>
+
+
+         <b-dropdown text="در باره ما" class="mr-auto">
+          <b-dropdown-item href="#">An item</b-dropdown-item>
+          <b-dropdown-item href="#">Another item</b-dropdown-item>
+      </b-dropdown>
+
+        </b-navbar>
+    </b-container>
+
+      <div id="nav">
+        <router-link to="/">Home</router-link>|
+        <router-link to="/about/10">About</router-link>
+      </div>
+      <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style lang="scss" scoped>
+.max-width{
+  max-width: 300px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 new Vue({
   router,
   store,
